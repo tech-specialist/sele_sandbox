9fb091b19811998d2ab7419bfddfd7d4a1c50575
Белов Дмитрий <108318627+tech-specialist@users.noreply.github.com>
2026-07-31 02:21:52 +0300

    Selenide with comments. Please, check it Natalia.

--- src/test/java/ProjectPage.java
+++ src/test/java/ProjectPage.java
@@ -1,17 +1,13 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebElementsCondition;
 import org.openqa.selenium.By;
-
 import java.time.Duration;
-
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
-import static java.awt.SystemColor.text;
 
 public class ProjectPage {
     final SelenideElement createBtn = $x("//*[text()='Create new project']");
@@ -25,18 +21,28 @@ public class ProjectPage {
     }
 
     public ProjectPage checkBtnColor() {
+        // 1. Проверяем, что цвет кнопки соответствует синему в формате RGBA
         createBtnV.shouldHave(cssValue("background-color", "rgba(64, 64, 220, 1)"));
+        // 2. Проверяем, что кнопка (или чекбокс) находится в состоянии "выбрано"
         createBtn.shouldHave(attribute("checked"));
-        bbb.shouldHave(size(6));
-        bbb.shouldHave(texts("jkjbj", "lhbkjb", "iihbhh"));
-        bbb.findBy(text("Shakira")).shouldBe(exist);
+        // 3. Проверки коллекции элементов bbb
+        bbb.shouldHave(size(6)); // В списке должно быть строго 6 элементов
+        bbb.shouldHave(texts("jkjbj", "lhbkjb", "iihbhh")); // Проверка текста у первых элементов
+        bbb.findBy(text("Sharelane")).shouldBe(exist); // В списке должен быть элемент с текстом Sharelane
 
+        //4. Это локатор. Он ищет элемент <span>, внутри которого находится текст "asdg"
         $(byTagAndText("span", "asdg"));
 
-        createBtn.getWrappedElement().sendKeys();
-        createBtn.getText();
+        // 5. Взаимодействие с элементом
+        createBtn.getWrappedElement().sendKeys(); // Имитация ввода (сейчас пустая)
+        createBtn.getText(); // Получение текста кнопки
         $(By.id("")).should(Condition.matchText("STG([1-9][0-9]{0,3})"));
+        /*$(By.id("...")) - Ищет элемент на странице по его уникальному атрибуту id.
+        Condition.matchText(...): Проверяет, что видимый текст элемента соответствует регулярному выражению (Regex).
+        Regex - регулярное выражение, когда мы прописываем не само значение, а формат.
+        "STG([1-9][0-9]{0,3})": Это шаблон регулярного выражения. Он ищет текст, который:Начинается со строгого префикса STG.
+        Дальше содержит число, где первая цифра — от 1 до 9 (не ноль).
+        После первой цифры может идти еще от 0 до 3 любых цифр (от 0 до 9).Примеры совпадений: STG1, STG54, STG100, STG9999.*/
         return this;
     }
-
 }
